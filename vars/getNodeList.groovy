632f9e14a779a0be59cbef706abd313785bddd6e
Rakesh Huliyar Ravishankar <30697765+Rakesh-HR@users.noreply.github.com>
2024-09-19 18:41:41 -0700

    Updated node list

--- vars/getNodeList.groovy
+++ vars/getNodeList.groovy
@@ -1,10 +1,33 @@
+// def call() {
+//     def nodes = []
+//     jenkins.model.Jenkins.get().computers.each { c ->
+//         if (c.node.labelString.contains("windows")) {
+//         nodes.add(c.node.selfLabel.name)
+//         }
+//     }
+//     sh "echo $nodes"
+//     return nodes
+// }
+
 def call() {
-    def nodes = []
+    def nodeInfo = [:]
     jenkins.model.Jenkins.get().computers.each { c ->
         if (c.node.labelString.contains("windows")) {
-        nodes.add(c.node.selfLabel.name)
+            def nodeName = c.node.selfLabel.name
+            def totalExecutors = c.numExecutors
+            def busyExecutors = c.countBusy()
+            def freeExecutors = totalExecutors - busyExecutors
+            
+            nodeInfo[nodeName] = [
+                total: totalExecutors,
+                free: freeExecutors
+            ]
         }
     }
-    sh "echo $nodes"
-    return nodes
+    
+    nodeInfo.each { nodeName, info ->
+        echo "Node: ${nodeName}, Total Executors: ${info.total}, Free Executors: ${info.free}"
+    }
+    
+    return nodeInfo
 }
